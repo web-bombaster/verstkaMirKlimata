053e9708ebddbd711ecd9ab2a41f7cb35cbedecd
web-bombaster <skorpion_05@inbox.ru>
2023-03-24 17:15:26 +0300

    Сверстана главная страница, пока без учета правок

---
+++ src/js/common.js
@@ -0,0 +1,17 @@
+
+document.addEventListener(
+	"DOMContentLoaded", () => {
+		// include './myscripts/preloader.js'
+		// include './myscripts/js-generation-code.js'
+		// include './myscripts/mobile-menu.js'
+		// include './myscripts/burger.js'
+		// include './myscripts/js-shop-cards.js'
+		// include './myscripts/toggle-menu.js'
+		// -include './myscripts/hidden-text.js'
+		// include './myscripts/form.js'
+		// include './myscripts/accordeon.js'
+		// include './myscripts/initSwiper.js'
+		// include './myscripts/blazy-init.js'
+		// -include './myscripts/colorsCollection.js'
+	}
+);
